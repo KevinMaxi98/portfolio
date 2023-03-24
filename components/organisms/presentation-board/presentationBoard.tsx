788ff3e76b818ce3c8f4a2ee2fc17031c98d8c4b
KEVIN ALEXANDER MAXI JARA <kmaxi@pichincha.com>
2023-03-24 12:31:23 -0500

    Fix mobile layout

--- components/organisms/presentation-board/presentationBoard.tsx
+++ components/organisms/presentation-board/presentationBoard.tsx
@@ -14,21 +14,25 @@ export default function PresentationBoard() {
     const SECONDARY_SUBTITLE = 'FULL STACK CAPABLE';
     return (
         <div className={styles.presentationBoard}>
-            <div>
-                <Title title={PRINCIPAL_TITLE_1}></Title>
-                <Title title={PRINCIPAL_TITLE_2}></Title>
-                <div className={styles.presentationBoard__description}>
-                    <Icon iconName={'remove'}></Icon>
-                    <Subtitle subtitle={PRINCIPAL_SUBTITLE}></Subtitle>
+            <div className={styles.presentationBoard__headerSection}>
+                <div>
+                    <Title title={PRINCIPAL_TITLE_1}></Title>
+                    <Title title={PRINCIPAL_TITLE_2}></Title>
+                    <div className={styles.presentationBoard__description}>
+                        <Icon className={styles.presentationBoard__icon} iconName={'remove'}></Icon>
+                        <Subtitle subtitle={PRINCIPAL_SUBTITLE}></Subtitle>
+                    </div>
+                </div>
+                <div className={styles.presentationBoard__imageSection}>
+                    <div className={styles.presentationBoard__imageContainer}>
+                        <Image src={profilePicture} fill alt={'Profile picture'}/>
+                    </div>
                 </div>
             </div>
-
-            <div className={styles.presentationBoard__imageSection}>
-                <Image src={profilePicture} width={251} height={256} alt={'Profile picture'}/>
-            </div>
             <div className={styles.presentationBoard__footerSection}>
-                <div className={`${styles.presentationBoard__description} ${styles['presentationBoard__description--secondary']}`}>
-                    <Icon iconName={'remove'}></Icon>
+                <div
+                    className={`${styles.presentationBoard__description} ${styles['presentationBoard__description--secondary']}`}>
+                    <Icon className={styles.presentationBoard__icon} iconName={'remove'}></Icon>
                     <Subtitle subtitle={SECONDARY_SUBTITLE}></Subtitle>
                 </div>
                 <Title textAlign={'end'} title={SECONDARY_TITLE_1}></Title>
